3585530a2f56ad20317b948d34f9a5c267850200
Diogo Sousa <dhesousa77@gmail.com>
2020-11-21 17:43:39 -0300

    Primeiro Commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro programa"); 
+
+console.log("Meu primeiro programa: trabalhando com variaveis")
+
+const idade = 26
+console.log("A idade que voce digitou foi de:", idade, "anos")
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+console.log(idade*2)
+
+const idadeSomada = idade+2
+console.log(idadeSomada)
